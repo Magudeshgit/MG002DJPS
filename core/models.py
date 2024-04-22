f8064c905ba87c460328e3143507d9744e7aadda
Magudeshgit <magudesh2006@gmail.com>
2024-04-23 00:46:12 +0530

    'billing'

--- core/models.py
+++ core/models.py
@@ -1,1 +1,52 @@
 from django.db import models
+import json
+
+# Customer Dependencies
+
+class stock(models.Model):
+    productname = models.CharField(max_length=70, db_index=True)
+    category = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.productname
+    
+class client(models.Model):
+    clientname = models.CharField(max_length=50)
+    contactnumber = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.clientname
+    
+# class billproduct(models.Model):
+#     products = models.ForeignKey(stock, on_delete=models.CASCADE)
+#     quantity = models.PositiveBigIntegerField(default=1)
+
+class bill(models.Model):
+    client = models.ManyToManyField(client)
+    billno = models.CharField(max_length=10)
+    products = models.JSONField()
+    # products = models.ManyToManyField(billproduct)
+
+    def __str__(self):
+        return self.billno
+    
+    def get_products(self):
+        return json.loads(self.products)
+
+    def set_products(self, products):
+        self.products = json.dumps(products)
+
+# Employee Dependencies
+
+class labor(models.Model):
+    laborname = models.CharField(max_length=50)
+    contactnumber = models.CharField(max_length=20)
+    address = models.TextField()
+    salarystatus = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.laborname
+    
--- core/views.py
+++ core/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import stock, client, bill, labor
+from django.db.models import Count
+import json
 
 
 def home(request):
     return HttpResponse("Home page")
 @login_required(login_url='/auth/signup/')
 def dashboard(request):
-    return render(request, "core/dashboard.html")   
+    model = stock.objects.all().order_by('productname')
+    most_retrieved_item = stock.objects.values('productname').annotate(count=Count('productname')).order_by('-count').first()
+    print(most_retrieved_item)
+
+    context = {
+        'overviewdata':model.reverse()[:5],
+        'stockcount': model.count(),
+        }
+    return render(request, "core/dashboard.html", context)   
 
 def stocks(request):
     return render(request, "core/stocks.html")
@@ -25,7 +36,14 @@ def bills(request):
     return render(request, "core/bills.html")
 
 def newbill(request):
-    return render(request, "core/newbill.html")
+    clients = client.objects.values_list('clientname')
+    stocks = stock.objects.values_list('productname')
+    
+    clients = json.dumps(list(clients.values()))
+    stocks = json.dumps(list(stocks.values()))
+
+    context = {"clients": clients, "products": stocks}
+    return render(request, "core/newbill.html", context)
 
 def clientbook(request):
     return render(request, "core/clientbook.html")
