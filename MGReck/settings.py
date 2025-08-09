e87cb6546ffc2b57413dc78b6acae41184db231a
Magudeshgit <magudesh2006@gmail.com>
2025-08-09 19:18:37 +0530

    new

--- MGReck/settings.py
+++ MGReck/settings.py
@@ -160,7 +160,7 @@ EMAIL_USE_TLS = True
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://957830fe2ab733437882a3d1ceb31f94@o4509814161670144.ingest.us.sentry.io/4509814164029440",
+    dsn="https://e17fefd8427c9f04eba3d08de9a7bd01@o4509814161670144.ingest.us.sentry.io/4509814432268288",
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     debug=True,
